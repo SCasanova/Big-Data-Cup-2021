11d598e7ca684336bdb3f2dcac25f7940c72af92
tejseth <55564043+tejseth@users.noreply.github.com>
2021-02-05 20:51:38 -0500

    Add files via upload

--- bdc_start.R
+++ bdc_start.R
@@ -1,5 +1,16 @@
 library(tidyverse)
 library(xgboost)
+library(magrittr)
+library(dplyr)
+library(Matrix)
+library(na.tools)
+library(ggimage)
+library(gt)
+library(mgcv)
+library(scales)
+library(ggforce)
+library(remotes)
+library(ggtext)
 
 scouting <- read.csv("~/Desktop/hackathon_scouting.csv")
 womens <- read.csv("~/Desktop/hackathon_womens.csv")
@@ -37,13 +48,78 @@ winners <- womens %>%
 womens <- womens %>%
   left_join(winners)
 
+womens <- womens %>%
+  mutate(score_diff = case_when(
+    Home.Team == Team ~ Home.Team.Goals - Away.Team.Goals,
+    Away.Team == Team ~ Away.Team.Goals - Away.Team.Goals
+  ))
+
+womens <- womens %>%
+  mutate(skater_diff = case_when(
+    Home.Team == Team ~ Home.Team.Skaters - Away.Team.Skaters,
+    Away.Team == Team ~ Away.Team.Skaters - Home.Team.Skaters
+  ))
+
 model_data <- womens %>%
   filter(winner != "TIE") %>%
   mutate(label = ifelse(Team == winner, 1, 0)) %>%
-  select()
+  select(label, Period, Clock, score_diff, skater_diff, total_time_left)
 
+smp_size <- floor(0.80 * nrow(model_data))
+set.seed(123)
+ind <- sample(seq_len(nrow(model_data)), size = smp_size)
+ind_train <- model_data[ind, ]
+ind_test <- model_data[-ind, ]
 
+nrounds = 100
+params <-
+  list(
+    booster = "gbtree",
+    objective = "binary:logistic",
+    eval_metric = c("logloss"),
+    eta = 0.2,
+    gamma = 0,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    max_depth = 4,
+    min_child_weight = 1
+  )
 
+full_train = xgboost::xgb.DMatrix(model.matrix(~.+0, data = ind_train %>% select(-label)),
+                                  label = ind_train$label)
 
+set.seed(123) 
+wp_model <- xgboost::xgboost(params = params, data = full_train, nrounds = nrounds, verbose = 2)
+
+importance <- xgboost::xgb.importance(feature_names = colnames(wp_model), model = wp_model)
+xgb.plot.importance(importance)
+
+model_games <- model_data %>%
+  mutate(index = 1:n())
+
+wp_games <- stats::predict(wp_model,
+                             as.matrix(model_games %>%
+                                         select(label, Period, Clock, score_diff, 
+                                                skater_diff, total_time_left))) %>%
+    tibble::as_tibble() %>%
+    dplyr::rename(prob = "value") %>%
+    dplyr::bind_cols(purrr::map_dfr(seq_along(model_games$index), function(x) {
+      tibble::tibble("wp" = 0:1,
+                     "Period" = model_games$Period[[x]],
+                     "Clock" = model_games$Clock[[x]],
+                     "score_diff" = model_games$score_diff[[x]],
+                     "skater_diff" = model_games$skater_diff[[x]],
+                     "total_time_left" = model_games$total_time_left[[x]],
+                     "index" = model_games$index[[x]]) 
+    })) %>%
+    dplyr::group_by(.data$index) %>%
+    dplyr::mutate(cum_prob = cumsum(.data$prob),
+                  prob = .data$prob) %>%
+    dplyr::select(-.data$cum_prob) %>%
+    dplyr::summarise(win_prob = sum(.data$prob * .data$wp)) %>%
+    ungroup()   
+  
+    
+  
 
 
